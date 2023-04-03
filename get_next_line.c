33eee03e2b925bd19e5f1883d2c43261b25a7e24
1delly <taz12pro@gmail.com>
2023-04-03 16:52:10 +0200

    hoo

--- get_next_line.c
+++ get_next_line.c
@@ -6,106 +6,13 @@
 /*   By: tdelgran <tdelgran@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 11:50:51 by tdelgran          #+#    #+#             */
-/*   Updated: 2023/04/01 18:30:46 by tdelgran         ###   ########.fr       */
+/*   Updated: 2023/04/03 16:51:25 by tdelgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
-
-// char	*read_line(char *line, int fd)
-// {
-// 	char	buffer[BUFFER_SIZE + 1];
-// 	int		bytes_read;
-
-// 	bytes_read = 1;
-// 	while (bytes_read > 0 && ft_strchr(line, '\n') == NULL)
-// 	{
-// 		bytes_read = read(fd, buffer, BUFFER_SIZE);
-// 		if (bytes_read == -1)
-// 			return (NULL);
-// 		buffer[bytes_read] = '\0';
-// 		line = ft_strjoin(line, buffer);
-// 	}
-// 	return (line);
-// }
-
-// char	*format_line(char *line, char *stock)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (line[i])
-// 	{
-// 		if (line[i] == '\n')
-// 		{
-// 			i++;
-// 			break ;
-// 		}
-// 		i++;
-// 	}
-// 	if (line[i] && stock != NULL)
-// 		stock = ft_strdup(line + i);
-// 	return (stock);		
-// }
-
-// char	*trim(char *str)
-// {
-// 	int	i;
-
-// 	if (str == NULL)
-// 		return (NULL);
-// 	i = 0;
-// 	while (str[i] != '\n' && str[i] != '\0')
-// 		i++;
-// 	if (str[i] == '\n')
-// 		return (ft_strdup(str + i));
-// 	return (str);
-// }
-
-// char	*get_next_line(int fd)
-// {
-// 	char		*line;
-// 	static char	*stock;
-
-// 	if (fd < 0 || BUFFER_SIZE <= 0)
-// 		return (NULL);
-// 	line = malloc(sizeof(char));
-// 	*line = '\0';
-// 	line = read_line(line, fd);
-// 	stock = format_line(line, stock);
-// 	line = trim(line);
-//     return (stock);
-// }
-
-// int	main(int ac, char **av)
-// {
-// 	int		i;
-// 	int		fd;
-// 	char	*str;
-// 	if (ac != 2)
-// 	{
-// 		printf("Wrong number of arguments !\n");
-// 		return (-1);
-// 	}
-// 	i = 0;
-// 	fd = open("coucou.txt", O_RDONLY);
-// 	if (fd == -1)
-// 	{
-// 		printf("open() failed\n");
-// 		return (0);
-// 	}
-// 	while (i++ < atoi(av[1]))
-// 	{
-// 		printf("\n**************** appel %d **************\n\n", i);
-// 		str = get_next_line(fd);
-// 		printf("%s", str);
-// 	}
-// 	if (close(fd) == -1)
-// 		printf("close() failed\n");
-// 	return (0);
-// }
 
 char *read_line(char *line, int fd)
 {
@@ -134,8 +41,8 @@ char *read_line(char *line, int fd)
 
 char	*format_line(char *line, char **stock)
 {
-	int	i;
-		char	*new_stock;
+	int     i;
+	char    *new_stock;
 
 	if (!line)
 		return (NULL);
@@ -178,10 +85,7 @@ char *trim(char *str)
     {
         result = ft_substr(str, 0, i + 1);
         if (!result)
-        {
-            free(str);
             return (NULL);
-        }
         free(str);
         return (result);
     }
@@ -203,8 +107,6 @@ char *get_next_line(int fd)
     {
         free(line);
         line = ft_strdup(stock);
-        if (!line)
-            return (NULL);
     }
     line = read_line(line, fd);
     if (line)
@@ -219,3 +121,31 @@ char *get_next_line(int fd)
     }
     return (line);
 }
+
+// int	main(int ac, char **av)
+// {
+// 	int		i;
+// 	int		fd;
+// 	char	*str;
+// 	if (ac != 2)
+// 	{
+// 		printf("Wrong number of arguments !\n");
+// 		return (-1);
+// 	}
+// 	i = 0;
+// 	fd = open("coucou.txt", O_RDONLY);
+// 	if (fd == -1)
+// 	{
+// 		printf("open() failed\n");
+// 		return (0);
+// 	}
+// 	while (i++ < atoi(av[1]))
+// 	{
+// 		printf("\n**************** appel %d **************\n\n", i);
+// 		str = get_next_line(fd);
+// 		printf("%s", str);
+// 	}
+// 	if (close(fd) == -1)
+// 		printf("close() failed\n");
+// 	return (0);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tdelgran <tdelgran@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 11:50:43 by tdelgran          #+#    #+#             */
-/*   Updated: 2023/04/01 18:25:33 by tdelgran         ###   ########.fr       */
+/*   Updated: 2023/04/03 13:58:46 by tdelgran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,10 @@ size_t	ft_strlen(const char *str);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strdup(const char *s1);
 char	*ft_strchr(const char *s, int c);
-char	*read_line(char *line, int fd);
+char    *read_line(char *line, int fd);
 char    *format_line(char *line, char **stock);
 char	*trim(char *str);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char *get_line_from_stock(char **stock);
 
 #endif
